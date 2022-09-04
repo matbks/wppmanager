5d029cb8dbcd38e1131a3442b9e261fea995ed54
matbks <mateus.bks@hotmail.com>
2022-09-03 22:16:21 -0300

    sebd

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,39 @@ import WppManager from "./wppsender"
 import { appendFile } from "fs";
 
 const wppManager = new WppManager()
+var port = process.env.PORT || 5000
+var router = express.Router()
 
 const app = express()
       app.use(express.json())
       app.use( express.urlencoded( { extended:false } ))
 
-console.log("vou dar app get")
 
-app.get( '/' , (req: Request, res: Response) => {
-    res.render("index.html");
-    console.log("vou dar app get")
-    res.send("Hello")
-    return response.json({message:'Server is up!'})
+router.use(function(req,res,next){
+    console.log('something is happening.')
+    next()
 })
+
+router.route('/send')
+.post( async (req: Request, res: Response) => {
+
+    console.log("vou dar send")
+    let dateTime = new Date()
+    let dateTimeStr = dateTime.toString()
+
+    try {
+        const { number, message } = req.body
+        await wppManager.sendText( number, message )
+    return res.status(200).json()
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).json({ status: "error", message: error })
+     }
+})
+
+
+
 
 app.post( '/send' , async (req: Request, res: Response) => {
 
