e06db16104f375906b6c6a51ca8670f26089a6cc
matbks <mateus.bks@hotmail.com>
2022-09-04 02:56:42 -0300

    ss

--- src/app.ts
+++ src/app.ts
@@ -10,27 +10,6 @@ const app = express()
       app.use(express.json())
 //   app.use( express.urlencoded( { extended:false } ))
 
-// router.route('/send')
-//     .post( async (req: Request, res: Response) => {
-
-//         console.log("vou dar send");
-
-//         try {
-//             const { number, message } = req.body
-//             await wppManager.sendText( number, message )
-//         return res.status(200).json();
-//         }
-//         catch(error){
-//             console.error(error);
-//             res.status(500).json({ status: "error", message: error })
-//         }
-//     })
-
-//     router.route('/')
-//     .get( (req: Request, res: Response) => {
-//         res.send("Yo!")
-//     })
-
 app.get('/', (request, response) => {
     response.send('Hello world!');
 });
