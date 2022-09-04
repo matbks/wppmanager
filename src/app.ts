6747c8a0f9568544a8a531bc34770f8e142e8542
matbks <mateus.bks@hotmail.com>
2022-09-03 23:14:26 -0300

    vv

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,39 @@
 import express, { Request, Response, Router} from "express"
 import WppManager from "./wppsender"
 import { appendFile } from "fs";
-import routes from './routes';
 
 const wppManager = new WppManager()
 var port = process.env.PORT || 5000
 var router = express.Router()
-// const router = Router();
 
 const app = express()
-    //   app.use(express.json())
-    //   app.use( express.urlencoded( { extended:false } ))
+//   app.use(express.json())
+//   app.use( express.urlencoded( { extended:false } ))
 
-      app.use('/api/', routes);
+// router.route('/send')
+//     .post( async (req: Request, res: Response) => {
 
-router.route('/send')
-    .post( async (req: Request, res: Response) => {
+//         console.log("vou dar send");
 
-        console.log("vou dar send");
+//         try {
+//             const { number, message } = req.body
+//             await wppManager.sendText( number, message )
+//         return res.status(200).json();
+//         }
+//         catch(error){
+//             console.error(error);
+//             res.status(500).json({ status: "error", message: error })
+//         }
+//     })
 
-        try {
-            const { number, message } = req.body
-            await wppManager.sendText( number, message )
-        return res.status(200).json();
-        }
-        catch(error){
-            console.error(error);
-            res.status(500).json({ status: "error", message: error })
-        }
-    })
+//     router.route('/')
+//     .get( (req: Request, res: Response) => {
+//         res.send("Yo!")
+//     })
 
-    router.route('/')
-    .get( (req: Request, res: Response) => {
-        res.send("Yo!")
-    })
-
+app.get('/', (request, response) => {
+    response.send('Hello world!');
+  });
 
 app.listen(port, () => {
     console.log(`App is listening at ${port}`)
