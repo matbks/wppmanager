d0182c2e9c84954e90418f5bbb9fddb526666176
matbks <mateus.bks@hotmail.com>
2022-09-03 22:56:41 -0300

    va

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Router} from "express"
 import WppManager from "./wppsender"
 import { appendFile } from "fs";
+import routes from './routes';
 
 const wppManager = new WppManager()
 var port = process.env.PORT || 5000
@@ -11,6 +12,7 @@ const app = express()
       app.use(express.json())
       app.use( express.urlencoded( { extended:false } ))
 
+      app.use('/api/', routes);
 
 router.use(function(req,res,next){
     console.log('something is happening.')
@@ -56,5 +58,7 @@ router.route('/send')
 //      }
 // })
 
-app.listen(port)
+app.listen(port, () => {
+    console.log(`App is listening at {process.ENV.PORT}`)
+  });
 // app.listen(0, () => console.log('Application is running'));
