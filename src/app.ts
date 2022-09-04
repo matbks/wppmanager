0692eacd638e52ad447b776311279f7ee8cd4e73
matbks <mateus.bks@hotmail.com>
2022-09-03 23:16:26 -0300

    tt

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router} from "express"
+import express, { Request, Response, Router } from "express"
 import WppManager from "./wppsender"
 import { appendFile } from "fs";
 
@@ -33,8 +33,23 @@ const app = express()
 
 app.get('/', (request, response) => {
     response.send('Hello world!');
-  });
+});
+
+app.post('/', (req: Request, res: Response) => {
+
+    console.log("vou dar send");
+
+    try {
+        const { number, message } = req.body
+        await wppManager.sendText(number, message)
+        return res.status(200).json();
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ status: "error", message: error })
+    }
+})
 
 app.listen(port, () => {
     console.log(`App is listening at ${port}`)
-  });
+});
