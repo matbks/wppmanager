cce65dd1afef30f85b536c2d4ea72faefef15936
matbks <mateus.bks@hotmail.com>
2022-09-04 15:09:32 -0300

    s

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,25 @@
-import { create, Whatsapp } from 'venom-bot';
+// Supports ES6
+import venom, { create, Whatsapp } from 'venom-bot'; 
 
-  create({
-    session: 'WppManager', //name of session
+ create({
+    session: 'session-name', //name of session
     multidevice: true // for version not multidevice use false.(default: true)
   })
-  .then((client) => start(client))
-  .catch((erro) => {
+  .then((client: any) => start(client))
+  .catch((erro: any) => {
     console.log(erro);
   });
 
-function start(client:Whatsapp) {
+function start(client:any) {
   client.onMessage((message:any) => {
     if (message.body === 'Hi' && message.isGroupMsg === false) {
       client
         .sendText(message.from, 'Welcome Venom 🕷')
         .then((result:any) => {
-          console.log('Result: ', result); 
+          console.log('Result: ', result); //return object success
         })
         .catch((erro:any) => {
-          console.error('Error when sending: ', erro); 
+          console.error('Error when sending: ', erro); //return object error
         });
     }
   });
